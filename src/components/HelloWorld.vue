51137b50495b6bdf7d541b5ab0ae6b5f14a393df
Misha Ovsepyan <m1shkaa666@gmail.com>
2020-07-14 19:38:47 +0300

    form removed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,26 +6,6 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
   </div>
 </template>
 
