b7dc20328028a5818a105d5f8cc77fd161498281
Mike <m1shkaa666@gmail.com>
2020-10-07 11:43:54 +0000

    testing github workspaces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App, Misha!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App, Misha!!"/>
   </div>
 </template>
 
